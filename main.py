3b2ed9236c78f63642baf12df48364831f432860
Daniel Thomas <business.danielthomas@outlook.com>
2026-03-04 16:06:03 +0000

    Improved SQLITEMODULE (Password Hashing,etc.) and added login functionality (Login/Register using SQLITEMODULE)

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response, Cookie, Depends, HTTPException
 from pydantic import BaseModel
-from sqliteModule import *
-import sqlite3
+from sqliteModule import Database
+from uuid import uuid4
 
+
+sqlite3Database = Database()
+sqlite3Database.createDatabase()
 
 app = FastAPI()
 
+# TODO: CHANGE IN-MEM SESSIONS INTO DB
+sessions = {}
+
+# TEMP
+def getCurrentUser(session_id: str = Cookie(default=None)):
+    if not session_id or session_id not in sessions:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    return sessions[session_id]
 
 class UserRegister(BaseModel):
     EmailAddress : str
+    Password: str
     Username: str
     DisplayName : str
+
+class UserLogin(BaseModel):
+    EmailAddress : str
     Password : str
 
 
@@ -23,7 +38,35 @@ def register(user: UserRegister):
     for field, value in user.model_dump().items():
         if not value or value.strip() == "":
             return {f"Missing Required Field: {field}"}
-    return "Ok"
+
+    databaseResponse = sqlite3Database.registerUser(user.EmailAddress, user.Password, user.Username, user.DisplayName)
+
+    return databaseResponse
+
+@app.post("/login")
+def login(user: UserLogin, response: Response):
+    for field, value in user.model_dump().items():
+        if not value or value.strip() == "":
+            return {f"Missing Required Field: {field}"}
+    # If correct. Response should look like:  {"Successful": True, "Response": "Correct Credentials", "EmailAddress": "email", "Username": "username", "DisplayName": "displayname"}
+    # TODO : CREATE A SESSION COOKIE HERE
+    databaseResponse = sqlite3Database.userLogin(user.EmailAddress, user.Password)
+
+    # TODO: ADD LOGIC IF RESPONSE FALSE FIRST!
+
+    session_id = str(uuid4())
+    sessions[session_id] = {
+    "EmailAddress": databaseResponse["EmailAddress"],
+    "Username": databaseResponse["Username"]}
+    response.set_cookie(key="session_id", value=session_id, httponly=True, max_age=1800, samesite="lax", secure=False)
+    return databaseResponse
+
+@app.get("/me")
+def get_me(user=Depends(getCurrentUser)):
+    return {
+        "EmailAddress": user["EmailAddress"],
+        "Username": user["Username"]
+    }
 
 # TODO:
 """
--- sqliteModule.py
+++ sqliteModule.py
@@ -1,4 +1,4 @@
-import sqlite3, json
+import sqlite3, bcrypt
 
 class Database:
     def __init__(self):
@@ -8,29 +8,40 @@ class Database:
         self.cursor = self.connection.cursor()
     
     def createDatabase(self):
-        query = "CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY, EmailAddress TEXT NOT NULL UNIQUE, Password TEXT NOT NULL, Username TEXT NOT NULL UNIQUE, DisplayName TEXT NOT NULL )"
+        query = "CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY, EmailAddress TEXT NOT NULL UNIQUE, Password BLOB NOT NULL, Username TEXT NOT NULL UNIQUE, DisplayName TEXT NOT NULL )"
         self.cursor.execute(query)
         self.connection.commit()
         query = "CREATE TABLE IF NOT EXISTS todoitems (todoitemID INTEGER PRIMARY KEY, userID INTEGER, title TEXT NOT NULL, description TEXT, FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE )"
         self.cursor.execute(query)
         self.connection.commit()
 
-
     def addTestData(self):
         try:
-            query = "INSERT INTO users(EmailAddress, Password, Username, DisplayName) VALUES (?,?,?,?)"
-            self.cursor.execute(query, ("test@user.com", "1234", "@test_user", "Test User",))
-            self.connection.commit()
-            return {"Successful": True, "Response": "Account Created."}
-
+            query = "SELECT count(*) FROM users"
+            self.cursor.execute(query)
+            count = self.cursor.fetchone()[0]
+            if count == 0:
+                # Only populate if table is empty.
+                query = "INSERT INTO users(EmailAddress, Password, Username, DisplayName) VALUES (?,?,?,?)"
+                self.cursor.execute(query, ("test@user.com", "1234", "@test_user", "Test User",))
+                self.connection.commit()
+            else:
+                return {"Successful": False, "Response": "Table is already populated."}
+                
+            return {"Successful": True, "Response": "Table Populated."}
         except Exception as e :
             return {"Successful": False, "Response": "[SQLITE] Backend Error (Add Test Data)"}
+    
     def userLogin(self, email_address, password):
-        query = "SELECT EmailAddress, Username, DisplayName FROM users WHERE EmailAddress = ? AND Password = ?"
-        self.cursor.execute(query, (email_address, password))
+        query = "SELECT EmailAddress, Password, Username, DisplayName FROM users WHERE EmailAddress = ?"
+        self.cursor.execute(query, (email_address,))
         row = self.cursor.fetchone()
         if row != None:
-            return {"Successful": True, "Response": "Correct Credentials. NOT YET IMPLEMENTED FUNCTIONALITY"}
+            databasePassword = row[1]
+            if bcrypt.checkpw(password.encode('utf-8'), databasePassword):
+                return {"Successful": True, "Response": "Correct Credentials", "EmailAddress": row[0], "Username": row[2], "DisplayName": row[3]}
+            else:
+                return {"Successful": False, "Response": "Incorrect Credentials"}
         else:
             return {"Successful": False, "Response": "Incorrect Credentials."}
         
@@ -45,12 +56,8 @@ class Database:
         rows = self.cursor.fetchone()
         if(rows):
             return {"Successful": False, "Response": "Username Taken."}
-        
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         query = "INSERT INTO users(EmailAddress, Password, Username, DisplayName) VALUES (?,?,?,?)"
-        self.cursor.execute(query, (email_address, password, username, display_name))
+        self.cursor.execute(query, (email_address, hashed_password, username, display_name,))
         self.connection.commit()
         return {"Successful": True, "Email": email_address, "Username": username}
-
-sqlite3Database = Database()
-sqlite3Database.createDatabase()
-
